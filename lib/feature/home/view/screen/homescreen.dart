54e5809174e833c0c7650fece887e96821ac40f7
Eman Ahmed <127321144+emanelnazer@users.noreply.github.com>
2025-11-26 21:36:00 +0200

    Add files via upload

---
+++ lib/feature/home/view/screen/homescreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:movie_app/features/home/view/widgets/popularWidget.dart';
+import 'package:movie_app/features/home/view/widgets/releaseWidget.dart';
+import 'package:movie_app/features/home/view_model/popular_cubit.dart';
+import 'package:movie_app/features/home/view_model/release_cubit.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = 'HomeScreen';
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late PopularCubit popularCubit;
+  late ReleaseCubit releaseCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    popularCubit = PopularCubit();
+    popularCubit.getMovies();
+    releaseCubit = ReleaseCubit();
+    releaseCubit.getMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          PouplarWidget(popularCubit: popularCubit),
+          SizedBox(
+            height: 10,
+          ),
+          ReleaseWidget(releaseCubit: releaseCubit),
+        ],
+      ),
+    ));
+  }
+}
